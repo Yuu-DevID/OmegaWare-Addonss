476999a0e7ac1a70307d9c1fd00fc9c3ddb2e14c
ユスフ - Yusuf <150532415+Yuu-DevID@users.noreply.github.com>
2026-02-17 11:57:30 +0700

    Update target version to 1_21_11

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("fabric-loom") version "1.12.7"
 }
 
-val targetVersion = findProperty("target_version") as String? ?: "1_21_10"
+val targetVersion = findProperty("target_version") as String? ?: "1_21_11"
 val minecraftVersion = properties["minecraft_version_$targetVersion"] as String
 val yarnMappings = properties["yarn_mappings_$targetVersion"] as String
 val loaderVersion = properties["loader_version_$targetVersion"] as String
